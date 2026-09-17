b0bfa3f5220747f40cbb07adf01a352f7c69de8d
yadav767 <yyadavaayush767@gmail.com>
2026-09-17 17:14:14 +0530

    Added frontent URL in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontent-chatgpt.vercel.app",
     credentials: true,
 }))
 
